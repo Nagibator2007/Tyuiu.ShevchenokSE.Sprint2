7c8e606a7f45b7817a43c532def03318cfbd5053
Nagibator2007 <shevchenokse@outlook.com>
2025-11-26 21:44:30 +0500

    Таск 3 чуваааааааааааааааак

---
+++ Tyuiu.ShevchenokSE.Sprint2.Task3.V25.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.ShevchenokSE.Sprint2.Task3.V25.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint2.Task3.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+
+        }
+
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+        }
+
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+        }
+
+        public void ValidCondition5()
+        {
+            DataService ds = new DataService();
+        }
+    }
+}
