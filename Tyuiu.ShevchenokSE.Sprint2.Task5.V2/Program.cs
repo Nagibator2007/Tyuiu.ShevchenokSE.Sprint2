b24a76a1378a5a949903b98be051ea7b2e4ef6a6
Nagibator2007 <shevchenokse@outlook.com>
2025-11-27 12:43:36 +0500

    таск5

---
+++ Tyuiu.ShevchenokSE.Sprint2.Task5.V2/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ShevchenokSE.Sprint2.Task5.V2.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint2.Task5.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Шевченок С.Е.  | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнила: Шевченок Сергей Евгеньевич | СМАРТб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* По данному месяцу, определите в какую пору года попадает этот месяц     *");
+            Console.WriteLine("* Зима, Лето, Весна, Осень).                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите номер месяца: ");
+            int numMouth = Convert.ToInt32(Console.ReadLine());
+            string res;
+            if ((numMouth < 1) || (numMouth > 12))
+            {
+                res = "Введенно неверное значение!";
+            }
+            else
+            {
+                res = "Этот меняц пренадлежит времени года: " + ds.FindMonthSeason(numMouth);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
