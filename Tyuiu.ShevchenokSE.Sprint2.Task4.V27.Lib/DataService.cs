bbfd1c749ab9beb2a6287a44fb141cdcdc852597
Nagibator2007 <shevchenokse@outlook.com>
2025-11-28 22:41:59 +0500

    Task4 fix

---
+++ Tyuiu.ShevchenokSE.Sprint2.Task4.V27.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.ShevchenokSE.Sprint2.Task4.V27.Lib
+{
+    public class DataService : ISprint2Task4V27
+    {
+        public double Calculate(double x, double y)
+        {
+            double z = (x - 10) > (y + 2) ? (x * (Math.Pow(((y + 2) / (x - 1)), x))) : ((y * y) - (2 / x));
+            return Math.Round(z, 3);
+        }
+    }
+}
